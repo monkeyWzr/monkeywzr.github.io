7fe10fe1a11c9a99296fc4f31d1f36c391e11555
monkeyWzr <monkeywzr@gmail.com>
2020-07-20 00:23:38 +0900

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,32 +6,21 @@
 #     exit 1;
 # fi
 
-git config --global push.default matching
-git config --global user.email "${GitHubEMail}"
-git config --global user.name "${GitHubUser}"
-
-git checkout --orphan master
-git reset --hard
-git commit --allow-empty -m "Initializing master branch"
-git push origin master
-git checkout src
-
 echo "Deleting old publication"
 rm -rf public
-mkdir public
-
-echo "Checking out master branch into public"
-git worktree add -f -B master public origin/master
-
-echo "Removing existing files"
-rm -rf public/*
 
 echo "Generating site"
 hugo
 
 echo "Updating master branch"
 cd public
-git add --all
+git init
+
+git config --global push.default matching
+git config --global user.email "${GitHubEMail}"
+git config --global user.name "${GitHubUser}"
+
+git add --all .
 git commit -m "Publishing to master (deploy.sh)"
 
 echo "Pushing to github"
